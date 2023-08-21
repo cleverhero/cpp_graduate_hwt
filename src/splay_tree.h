7c05343f8d2b56e38ceec0560a9cb600b2f7b26e
Oleg_Prikhodko <oldmpr@rambler.ru>
2023-08-21 23:07:39 +1000

    Improve code style and performance

--- src/splay_tree.h
+++ src/splay_tree.h
@@ -24,7 +24,7 @@ namespace hwt
         using BST::nodes;
         using BST::root_id;
 
-        using BST::set_root;
+        using BST::update_node;
 
         using BST::get_right_id;
         using BST::get_left_id;
@@ -37,6 +37,103 @@ namespace hwt
         using BST::left_rotate;
 
 
+    private:
+        struct TmpTree {
+            id_opt_t head;
+            id_opt_t tail;
+        };
+
+        id_t pure_left_rotate(id_t topnode_id) noexcept {
+            id_t new_topnode_id = get_right_id(topnode_id).value();
+
+            auto rl_id = get_left_id(new_topnode_id);
+            nodes[topnode_id]->right_id = rl_id;
+            if (rl_id)
+                nodes[rl_id.value()]->parent_id = topnode_id;
+
+            nodes[new_topnode_id]->left_id = topnode_id;
+            nodes[topnode_id]->parent_id = new_topnode_id;
+
+            update_node(topnode_id);
+            update_node(new_topnode_id);
+            return new_topnode_id;
+        };
+
+        id_t pure_right_rotate(id_t topnode_id) noexcept {
+            id_t new_topnode_id = get_left_id(topnode_id).value();
+
+            auto lr_id = get_right_id(new_topnode_id);
+            nodes[topnode_id]->left_id = lr_id;
+            if (lr_id)
+                nodes[lr_id.value()]->parent_id = topnode_id;
+
+            nodes[new_topnode_id]->right_id = topnode_id;
+            nodes[topnode_id]->parent_id = new_topnode_id;
+
+            update_node(topnode_id);
+            update_node(new_topnode_id);
+            return new_topnode_id;
+        };
+
+        void to_right_tree(id_opt_t node_id, TmpTree& right) noexcept {
+            if (!right.head)
+                right.head = node_id;
+            else {
+                nodes[right.tail.value()]->left_id = node_id;
+                if (node_id)
+                    nodes[node_id.value()]->parent_id = right.tail;
+
+                update_node(right.tail.value());
+            }
+            right.tail = node_id;
+        };
+
+        void to_left_tree(id_opt_t node_id, TmpTree& left) noexcept {
+            if (!left.head)
+                left.head = node_id;
+            else {
+                nodes[left.tail.value()]->right_id = node_id;
+                if (node_id)
+                    nodes[node_id.value()]->parent_id = left.tail;
+
+                update_node(left.tail.value());
+            }
+            left.tail = node_id;
+        };
+
+        id_t break_left(id_t topnode_id, TmpTree& left) noexcept {
+            id_t new_topnode_id = get_right_id(topnode_id).value();
+            nodes[topnode_id]->right_id = nullopt;
+
+            to_left_tree(topnode_id, left);
+
+            update_node(topnode_id);
+            return new_topnode_id;
+        };
+
+        id_t break_right(id_t topnode_id, TmpTree& right) noexcept {
+            id_t new_topnode_id = get_left_id(topnode_id).value();
+            nodes[topnode_id]->left_id = nullopt;
+
+            to_right_tree(topnode_id, right);
+
+            update_node(topnode_id);
+            return new_topnode_id;
+        };
+
+        void assemble(TmpTree& left, TmpTree& right) noexcept {
+            auto left_id = get_left_id(root_id.value());
+            to_left_tree(left_id, left);
+
+            auto right_id = get_right_id(root_id.value());
+            to_right_tree(right_id, right);
+
+            nodes[root_id.value()]->left_id = left.head;
+            nodes[root_id.value()]->right_id = right.head;
+            this->update_node(root_id.value());
+        }
+
+
     public:
         bool find(int key) override {
             if (!root_id)
@@ -46,81 +143,39 @@ namespace hwt
             return (nodes[root_id.value()]->key == key);
         }
 
-        void splay(int key) {
-            if (!root_id)
-                return;
+        void splay(int key) noexcept {
+            TmpTree tmp_left, tmp_right;
 
-            id_opt_t right_min = nullopt;
-            id_opt_t right_head = nullopt;
-            id_opt_t left_max = nullopt;
-            id_opt_t left_head = nullopt;
-
-            while (true) {
+            while(true) {
                 id_t curr_root_id = root_id.value();
-                auto left_id = get_left_id(curr_root_id);
-                auto right_id = get_right_id(curr_root_id);
                 auto root_key = nodes[curr_root_id]->key;
 
                 if (key < root_key) {
+                    auto left_id = get_left_id(curr_root_id);
                     if (!left_id)
                         break;
-                    if (key < nodes[left_id.value()]->key) {
-                        set_root(right_rotate(curr_root_id));
-                        curr_root_id = root_id.value();
 
-                        if (!get_left_id(curr_root_id))
-                            break;
-                    }
-                    if (!right_min)
-                        right_head = root_id;
-                    else
-                        link_left(right_min.value(), root_id);
-
-                    right_min = root_id;
-                    set_root(get_left_id(curr_root_id).value());
-                    curr_root_id = root_id.value();
-
-                    if (right_min)
-                        link_left(right_min.value(), nullopt);
+                    if (key < nodes[left_id.value()]->key)
+                        curr_root_id = pure_right_rotate(curr_root_id);
+                    curr_root_id = break_right(curr_root_id, tmp_right);
                 }
                 else if (key > root_key) {
+                    auto right_id = get_right_id(curr_root_id);
                     if (!right_id)
                         break;
-                    if (key > nodes[right_id.value()]->key) {
-                        set_root(left_rotate(curr_root_id));
-                        curr_root_id = root_id.value();
 
-                        if (!get_right_id(curr_root_id))
-                            break;
-                    }
-
-                    if (!left_max)
-                        left_head = root_id;
-                    else
-                        link_right(left_max.value(), root_id);
-
-                    left_max = root_id;
-                    set_root(get_right_id(curr_root_id).value());
-                    curr_root_id = root_id.value();
-
-                    if (left_max)
-                        link_right(left_max.value(), nullopt);
+                    if (key > nodes[right_id.value()]->key)
+                        curr_root_id = pure_left_rotate(curr_root_id);
+                    curr_root_id = break_left(curr_root_id, tmp_left);
                 }
                 else
                     break;
+
+                root_id = curr_root_id;
+                nodes[curr_root_id]->parent_id = nullopt;
             }
-            if (!left_max)
-                left_head = get_left_id(root_id.value());
-            else
-                link_right(left_max.value(), get_left_id(root_id.value()));
 
-            if (!right_min)
-                right_head = get_right_id(root_id.value());
-            else
-                link_left(right_min.value(), get_right_id(root_id.value()));
-
-            link_left(root_id.value(), left_head);
-            link_right(root_id.value(), right_head);
+            assemble(tmp_left, tmp_right);
         }
 
     public:
